97ca9ccba0b87b75e86db0d4b806dab4d4186004
Wesley Miranda <wymirandac@gmail.com>
2025-04-17 13:45:21 -0400

    [feat]: implementa animação de digitação com Typed.js
    - Adiciona inicialização dinâmica do Typed.js

---
+++ assets/js/main.js
@@ -0,0 +1,14 @@
+/*INICIAR TYPED.JS*/
+const selectTyped = document.querySelector('.typed')
+if (selectTyped){
+    let typed_strings = selectTyped.getAttribute('data-typed-items');
+    typed_strings = typed_strings.split(', ');
+    new Typed('.typed', {
+        strings: typed_strings,
+        loop: true,
+        typeSpeed: 100,
+        backSpeed: 50,
+        backDelay: 2000
+}
+    )
+}
